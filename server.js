422d4ed9ba70c8699238e8075795e14f9ede9e79
Kaleigh Herold <kaleigh224@gmail.com>
2019-02-21 20:22:56 -0500

    got handlebars set up. also got articles with links, titles, and summaries displayed as well as saved into DB. Still need to create on clicks for the comment buttons.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var logger = require("morgan");
-
+var mongojs = require("mongojs");
+var exphbs = require("express-handlebars");
 
 var axios = require("axios");
 var cheerio = require("cheerio");
@@ -12,7 +13,10 @@ var app = express();
 
 // Database configuration
 var databaseUrl = "newsScrape";
-var collections = ["articles"];
+var collections = ["scrapeData", "note"];
+
+
+var db = mongojs(databaseUrl, collections);
 
 // Middleware
 // Use morgan logger for logging requests
@@ -22,9 +26,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
+// Set handlebars as the default templating engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+db.on("error", function (error) {
+    console.log("Database Error:", error);
+})
 
 
 // Routes
+app.get("/", function (req, res) {
+    db.scrapeData.find({}, function (error, found) {
+        if (error) {
+            console.log(error)
+        } else {
+            res.render("index", { articles: found })
+        }
+    })
+})
+
 app.get("/scrape", function (req, res) {
     axios.get("https://www.heraldonline.com/news/local/community/fort-mill-times/").then(function (response) {
         var $ = cheerio.load(response.data);
@@ -42,7 +63,8 @@ app.get("/scrape", function (req, res) {
                 db.scrapeData.insert({
                     title: result.title,
                     link: result.link,
-                    summary: result.summary
+                    summary: result.summary,
+                    note: "none"
                 },
                     function (err, inserted) {
                         if (err) {
@@ -60,6 +82,20 @@ app.get("/scrape", function (req, res) {
     });
 });
 
-app.listen(PORT, function() {
+app.post("/save", function(req, res) {
+db.note.insert(req.body, function(error, saved) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        db.scrapeData.findOneAndUpdate(
+            { _id: req.params.id },
+            { $set: { note: req.body } }
+         )    
+    }
+})
+})
+
+app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
-  });
+});
