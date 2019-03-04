785915b757e0bed2ad15677b92ae4026b99ef32c
Kaleigh Herold <kaleigh224@gmail.com>
2019-03-04 12:26:15 -0500

    updated PORT

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ var logger = require("morgan");
 var mongojs = require("mongojs");
 var exphbs = require("express-handlebars");
 
+
 var axios = require("axios");
 var cheerio = require("cheerio");
 
 // var db = require("./models");
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
